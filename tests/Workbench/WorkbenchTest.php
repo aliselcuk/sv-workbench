da6bc47c7448024876e533fabef3970cdc4f4093
Ali Selcuk <maselcuk@gmail.com>
2019-07-07 06:58:42 +0300

    Replace @test with test__

--- tests/Workbench/WorkbenchTest.php
+++ tests/Workbench/WorkbenchTest.php
@@ -4,8 +4,7 @@ namespace Tests\Workbench;
 
 class WorkbenchTest extends TestCase
 {
-    /** @test */
-    function module_is_installed()
+    function test__module_is_installed()
     {
         $this->assertNotNull(superv('addons')->get('superv.modules.workbench'));
     }
